1be54fa4ed761b19f30375a0126332804815540a
hawkbee <googandads@gmail.com>
2019-09-20 17:13:29 +0200

    [green] title is Where Assistant

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'plugins/url_launcher/url_launcher.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   final title = 'Where Assistant';
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.brown,
         secondaryHeaderColor: Colors.green,
       ),
@@ -31,40 +21,16 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    final ThemeData themeData = Theme.of(context);
-    final TextStyle aboutTextStyle = themeData.textTheme.body2;
-    final TextStyle linkStyle = themeData.textTheme.body2.copyWith(color: themeData.accentColor);
-    final String url = 'http://google.com';
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Stack(
           children: <Widget>[
             Container(child: Image.asset('assets/icon-192x192.png')),
@@ -86,36 +52,31 @@ class _MyHomePageState extends State<MyHomePage> {
                 Tooltip(
                   message: 'Get it on Google Play',
                   child: InkWell(
-                      child: Image.asset('assets/google-play-badge.png'),
-                  onTap: () {
-                        try {
-                          UrlUtils.open('https://play.google.com/store/apps/details?id=io.whereassistant.whereassistant&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1');
-                        } catch (e) {
-                          print('Error -> $e');
-                        }
-                  },),
+                    child: Image.asset('assets/google-play-badge.png'),
+                    onTap: () {
+                      try {
+                        UrlUtils.open(
+                            'https://play.google.com/store/apps/details?id=io.whereassistant.whereassistant&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1');
+                      } catch (e) {
+                        print('Error -> $e');
+                      }
+                    },
+                  ),
                 ),
                 Center(
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: RichText(
-                      text: TextSpan(
-                        children: <TextSpan>[
-                          TextSpan(
-                            style: aboutTextStyle,
-                            text: 'When tidying up your things you just take pictures of it, where you put it. '
-                              "Later when you need your things but don't remember where it is... just ask your Where Assistant. It’s amazing with speech recognition.\n"
-                          ),
-                        ],
-                      ),
-                    ),
+                    child: Text(
+                        'When tidying up your things you just take pictures of it, where you put it. '
+                        "Later when you need your things but don't remember where it is... just ask your Where Assistant. It’s amazing with speech recognition.\n"),
                   ),
                 ),
                 Row(
                   children: <Widget>[
-                    Flexible(child: Image.asset('assets/Logo_French_Tech.svg.png')),
-                    Flexible(child: Image.asset('assets/normandy_french_tech.jpg')),
-
+                    Flexible(
+                        child: Image.asset('assets/Logo_French_Tech.svg.png')),
+                    Flexible(
+                        child: Image.asset('assets/normandy_french_tech.jpg')),
                   ],
                 ),
               ],
@@ -126,33 +87,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-class _LinkTextSpan extends TextSpan {
-
-  // Beware!
-  //
-  // This class is only safe because the TapGestureRecognizer is not
-  // given a deadline and therefore never allocates any resources.
-  //
-  // In any other situation -- setting a deadline, using any of the less trivial
-  // recognizers, etc -- you would have to manage the gesture recognizer's
-  // lifetime and call dispose() when the TextSpan was no longer being rendered.
-  //
-  // Since TextSpan itself is @immutable, this means that you would have to
-  // manage the recognizer from outside the TextSpan, e.g. in the State of a
-  // stateful widget that then hands the recognizer to the TextSpan.
-
-  _LinkTextSpan({ TextStyle style, String url, String text }) : super(
-      style: style,
-      text: text ?? url,
-      recognizer: TapGestureRecognizer()..onTap = () {
-        try {
-          UrlUtils.open(url);
-        } catch (e) {
-          print('Error -> $e');
-        }
-
-      }
-  );
-}
-
